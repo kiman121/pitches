3672c7c4ba39a58a87893c2920204c34ec4b5c7e
Patrick Kimanthi <kimanthip@gmail.com>
2021-08-14 09:49:56 +0300

    add the route view

---
+++ app/main/views.py
@@ -0,0 +1,11 @@
+from flask import render_template, redirect, url_for, request
+from . import main
+
+
+@main.route('/')
+def index():
+    '''
+    View function that returns the index page
+    '''
+    title = "Pitches|home"
+    return render_template('index.html', title=title)
