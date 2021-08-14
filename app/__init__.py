495fb0a0cd47845c80caac780ff4e65a3e0530fa
Patrick Kimanthi <kimanthip@gmail.com>
2021-08-14 09:21:08 +0300

    add main blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -22,4 +22,11 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
 
+    # Register blueprints
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+
+
+
     return app
