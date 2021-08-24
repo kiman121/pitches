3e3500a87517d76a936528efda11059179a0334e
Patrick Kimanthi <kimanthip@gmail.com>
2021-08-24 13:35:38 +0300

    add export of app variables into the environment

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
-# export SECRET_KEY='bcc17b4e40a185f17a364ebae0'
+# export SECRET_KEY=secrets.token_hex(16)
+export MAIL_USERNAME="mwango2047@gmail.com"
+export MAIL_PASSWORD="Moringaschool@2021"
 
 python3 manage.py server
